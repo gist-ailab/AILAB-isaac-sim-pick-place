95b398075bbdd93b5a8549de66be42d6fb17d192
mhko1998 <80435528+mhko1998@users.noreply.github.com>
2023-02-12 22:22:19 +0900

    Add files via upload

--- ur5e_pick_place.py
+++ ur5e_pick_place.py
@@ -14,18 +14,14 @@ simulation_app = SimulationApp({"headless": False})
 from omni.isaac.core import World
 from omni.isaac.manipulators import SingleManipulator
 from omni.isaac.manipulators.grippers import ParallelGripper
-from omni.isaac.core.utils.stage import add_reference_to_stage, get_current_stage
+from omni.isaac.core.utils.stage import add_reference_to_stage
 from pick_place_controller import PickPlaceController
 from omni.isaac.core.objects import DynamicCuboid
-from omni.isaac.sensor import Camera
-from omni.isaac.core.utils.semantics import add_update_semantics
 import carb
 import sys
 import numpy as np
 import argparse
 import os
-import matplotlib.pyplot as plt
-import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--test", default=False, action="store_true", help="Run in test mode")
@@ -34,7 +30,7 @@ args, unknown = parser.parse_known_args()
 my_world = World(stage_units_in_meters=1.0)
 my_world.scene.add_default_ground_plane()
 
-ur5e_usd_path = "workspace/ur5e_handeye_gripper.usd"
+ur5e_usd_path = "/home/ailab/Workspace/minhwan/isaac_sim-2022.2.0/github_my/isaac-sim-pick-place/ur5e_handeye_gripper.usd"
 if os.path.isfile(ur5e_usd_path):
     pass
 else:
@@ -53,19 +49,6 @@ my_ur5e = my_world.scene.add(
         prim_path="/World/UR5e", name="my_ur5e", end_effector_prim_name="right_inner_finger_pad", gripper=gripper
     )
 )
-# my_ur5e.set_joints_default_state(
-#     positions=np.array([-np.pi / 2, -np.pi / 2, -np.pi / 2, -np.pi / 2, np.pi / 2, 0])
-# )
-rgb_camera = Camera(
-    prim_path="/World/UR5e/realsense/RGB",
-    frequency=20,
-    resolution=(1920, 1080),
-)
-depth_camera = Camera(
-    prim_path="/World/UR5e/realsense/Depth",
-    frequency=20,
-    resolution=(1920, 1080),
-)
 size_scale = 0.03
 size_scale_z = 0.03
 cube = my_world.scene.add(
@@ -75,27 +58,13 @@ cube = my_world.scene.add(
         prim_path="/World/Cube",
         scale=np.array([size_scale, size_scale, size_scale]),
         size=1.0,
-        color=np.array([1, 0, 0]),
+        color=np.array([0, 0, 1]),
         mass=0
     )
 )
 my_world.scene.add_default_ground_plane()
 my_ur5e.gripper.set_default_state(my_ur5e.gripper.joint_opened_positions)
 my_world.reset()
-rgb_camera.initialize()
-depth_camera.initialize()
-
-rgb_camera.add_distance_to_camera_to_frame()
-rgb_camera.add_instance_segmentation_to_frame()
-rgb_camera.add_bounding_box_2d_loose_to_frame()
-rgb_camera.add_bounding_box_2d_tight_to_frame()
-depth_camera.add_distance_to_camera_to_frame()
-depth_camera.add_instance_segmentation_to_frame()
-# depth_camera.set_horizontal_aperture(0.05)
-
-stage = get_current_stage()
-cube_prim = stage.DefinePrim("/World/Cube")
-add_update_semantics(prim=cube_prim, semantic_label="cube")
 
 my_controller = PickPlaceController(
     name="pick_place_controller", gripper=my_ur5e.gripper, robot_articulation=my_ur5e, end_effector_initial_height=0.5
@@ -109,42 +78,12 @@ while simulation_app.is_running():
         if my_world.current_time_step_index == 0:
             my_world.reset()
             my_controller.reset()
-        # elif my_world.current_time_step_index == 200:
-        if my_controller._event == 0:
-            if my_controller._t >= 0.99:
-            # if my_world.current_time_step_index > 110:
-                my_controller.pause()
-                # print(my_controller._t)
-                # print(my_world.current_time_step_index)
-                rgb_image = rgb_camera.get_rgba()[:, :, :3]
-                depth_image = depth_camera.get_current_frame()["distance_to_camera"]
-                instance_segmentation_image = rgb_camera.get_current_frame()["instance_segmentation"]["data"]
-                # loose_bbox = rgb_camera.get_current_frame()["bounding_box_2d_loose"]
-                tight_bbox = rgb_camera.get_current_frame()["bounding_box_2d_tight"]
-                # print(loose_bbox)
-                print(tight_bbox)
-                
-                imgplot = plt.imshow(rgb_image)
-                plt.show()
-                inssegplot = plt.imshow(instance_segmentation_image)
-                plt.show()
-                depthplot = plt.imshow(depth_image)
-                plt.show()
-                
-                rgb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB)
-                instance_segmentation_image = np.array(instance_segmentation_image, dtype=type(depth_image[0][0]))
-                cv2.imwrite('/home/nam/.local/share/ov/pkg/isaac_sim-2022.2.0/workspace/data/rgb_image.png', rgb_image)
-                cv2.imwrite('/home/nam/.local/share/ov/pkg/isaac_sim-2022.2.0/workspace/data/depth_image.png', depth_image)
-                cv2.imwrite('/home/nam/.local/share/ov/pkg/isaac_sim-2022.2.0/workspace/data/mask.png', instance_segmentation_image)
-                
-                my_controller.resume()
-        # elif my_world.current_time_step_index > 210:
         observations = my_world.get_observations()
         actions = my_controller.forward(
             picking_position=cube.get_local_pose()[0],
             placing_position=np.array([0.4, -0.33, 0.02]),
             current_joint_positions=my_ur5e.get_joint_positions(),
-            end_effector_offset=np.array([0, 0, 0.25]),
+            end_effector_offset=np.array([0, 0, 0.25-size/2]),
         )
         if my_controller.is_done():
             print("done picking and placing")
