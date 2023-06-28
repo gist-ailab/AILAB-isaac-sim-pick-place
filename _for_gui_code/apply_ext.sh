f13e0e45476e8287d7fc594f1bec297ce575f257
ailabktw <ailab.ktw@gm.gist.ac.kr>
2023-06-28 20:52:39 +0900

    feat: update gui code applier

--- _for_gui_code/apply_ext.sh
+++ _for_gui_code/apply_ext.sh
@@ -1,6 +1,14 @@
 # ailab extension apply bash
 
-cp -Rv ./workspace/_for_gui_code/ailab_examples ./exts/omni.isaac.examples/omni/isaac/examples/
-cp -Rv ./workspace/_for_gui_code/ailab_script ./exts/omni.isaac.examples/omni/isaac/examples/
-cp -v ./workspace/_for_gui_code/extension.toml ./exts/omni.isaac.examples/config/
-
+{
+cp -Rv ./AILAB-isaac-sim-pick-place/_for_gui_code/ailab_examples ./exts/omni.isaac.examples/omni/isaac/examples/ \
+&&
+cp -Rv ./AILAB-isaac-sim-pick-place/_for_gui_code/ailab_script ./exts/omni.isaac.examples/omni/isaac/examples/ \
+&&
+cp -v ./AILAB-isaac-sim-pick-place/_for_gui_code/extension.toml ./exts/omni.isaac.examples/config/ \
+&&
+echo "\n\n.. apply complete\n"
+} ||
+{
+echo "\n\n!! apply fail !!\n"
+}
