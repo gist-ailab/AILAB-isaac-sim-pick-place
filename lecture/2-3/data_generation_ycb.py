e396db49c67f992b3badc6f722d90f1172029870
raeyo <raeyo.kang@gmail.com>
2023-07-03 14:03:32 +0900

    refactor lecture 2-3

--- lecture/2-3/data_generation_ycb.py
+++ lecture/2-3/data_generation_ycb.py
@@ -1,163 +1,139 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-#
+# ---- ---- ---- ----
+# GIST-AILAB, 2023 summer school
+# Day2. 
+# 2-3.2 Data generation with YCB dataset 
+# ---- ---- ---- ----
 
+#-----0. preliminary -----#
+
+# python path setting
+import os
+import sys
+lecture_path = os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) # path to lecture
+sys.path.append(lecture_path)
+
+# import packages
+from PIL import Image
+import numpy as np
+import random
+
+# function for save image
+def save_image(image, path):
+    image = Image.fromarray(image)
+    image.save(path)
+
+# data and save path
+robot_path = os.path.join(lecture_path, '/utils/tasks/ur5e_handeye_gripper.usd')
+data_path = os.path.join(lecture_path, '/dataset/ycb')
+save_path = os.path.join(lecture_path, '/detect_img')
+
+# get object list
+obj_dirs = [os.path.join(data_path, obj_name) for obj_name in os.listdir(data_path)]
+obj_dirs.sort()
+object_info = {}
+total_object_num = len(obj_dirs)
+for obj_idx, obj_dir in enumerate(obj_dirs):
+    usd_file = os.path.join(obj_dir, 'final.usd')
+    object_info[obj_idx] = {
+        'name': os.path.basename(obj_dir),
+        'usd_file': usd_file,
+        'label': obj_idx+2, # set object label 2 ~ 
+    }
+
+#-----1. Initialize simulation app and import packages
 from omni.isaac.kit import SimulationApp
-
 simulation_app = SimulationApp({"headless": False})
 
 from omni.isaac.core import World
-from omni.isaac.manipulators import SingleManipulator
-from omni.isaac.manipulators.grippers import ParallelGripper
-from omni.isaac.core.utils.stage import add_reference_to_stage, get_current_stage
+from omni.isaac.core.utils.semantics import add_update_semantics
+from omni.isaac.core.utils.stage import get_current_stage
 from omni.isaac.core.utils.prims import create_prim, delete_prim
 from omni.isaac.core.utils.semantics import add_update_semantics
-from omni.isaac.core.utils.extensions import get_extension_path_from_name
-from omni.isaac.core.objects import DynamicCuboid
-from omni.isaac.sensor import Camera
-from omni.isaac.core.prims.xform_prim import XFormPrim
-import omni.kit.commands
-from omni.isaac.core.utils.rotations import euler_angles_to_quat
-import sys
-sys.path.append("/home/ailab/Workspace/minhwan/isaac_sim-2022.2.0/AILAB-isaac-sim-pick-place/lecture/4-1")
-from reach_target_controller import ReachTargetController
-from pick_place_controller import PickPlaceController
-
-# from detection.inference_detection import inference_detection
-from omni.isaac.examples.ailab_script import AILabExtension
-from omni.isaac.examples.ailab_examples import AILab
-
-from detection import YCBDataset, get_model_instance_segmentation, get_transform
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
-from correct_radial_distortion import depth_image_from_distance_image
-# from ggcnn.inferece_ggcnn import inference_ggcnn
-
-import carb
-import numpy as np
-import argparse
-import os
-import matplotlib.pyplot as plt
-import cv2
-from PIL import Image, ImageDraw
-import glob
-import random
-import torch
-from pathlib import Path
-
-
-from utils.tasks.pick_place_vision_task import UR5ePickPlace
-import coco.transforms as T
-import torchvision.transforms as trans
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--robot_path",
-    type=str,
-    default="/home/ailab/Workspace/minhwan/isaac_sim-2022.2.0/AILAB-isaac-sim-pick-place/lecture/utils/tasks/ur5e_handeye_gripper.usd",
-    help="robot usd path.",
-)
-parser.add_argument(
-    "--data_path",
-    type=str,
-    default="/home/ailab/Workspace/minhwan/isaac_sim-2022.2.0/AILAB-isaac-sim-pick-place/lecture/dataset/ycb",
-    help="data usd directory",
-)
-parser.add_argument(
-    "--save_path",
-    type=str,
-    default="/home/ailab/Workspace/minhwan/isaac_sim-2022.2.0/detect_img",
-    help="img save path directory",
-)
-
-args = parser.parse_args()
 
 my_world = World(stage_units_in_meters=1.0)
-my_task = UR5ePickPlace(objects_list = [])  # releasing offset at the target position
 
+#-----2. Define Task and add to the world
+"""
+UR5ePickPlace
+- custom task for pick and place with UR5e + attached camera
+- TODO: summary of the task detail
+"""
+from utils.tasks.pick_place_vision_task import UR5ePickPlace
+# add task to the world
+my_task = UR5ePickPlace(objects_list = [])  # releasing offset at the target position
 my_world.add_task(my_task)
 my_world.reset()
 
+# get robot, camera, stage from task
 task_params = my_task.get_params()
+
 my_ur5 = my_world.scene.get_object(task_params["robot_name"]["value"])
-
 hand_camera = my_task.get_camera()
-
 stage = get_current_stage()
 
+
+#-----3. Add objects
+
+
+#-----4. Simulation loop
 i = 0
-objects = glob.glob(args.data_path+"/*/*.usd")
-
-transform = trans.ToPILImage()
-
 while simulation_app.is_running():
     my_world.step(render=True)
-    # random 1 ~ 3 data generation in camera boundary
-    
-    
-    
-    
-    
-    
-    
-    if i % 15 == 1:
-        obj_num = random.randint(1,3)
-        for l in range(obj_num):
-            pos_y = (random.random()*0.6-0.17)
-            pos_x = (random.random()*0.3+0.33)
-            pos_z = 0.00
-            a = random.randint(0, len(objects)-1)
-            object_prim = create_prim(usd_path=objects[a], prim_path="/World/object"+str(l), position=[pos_x,pos_y,pos_z], scale=[0.2,0.2,0.2])
-            # update semantic information with label 0 is unlabel 1 is background label go for 2 ~
-            add_update_semantics(prim=object_prim, semantic_label=str(l*100+a+2))
-        
-    
-    
-    
-    
-    if i % 15== 10:
-        
-        hand_rgb_image = hand_camera.get_rgba()[:, :, :3]
-        hand_depth_image = hand_camera.get_current_frame()["distance_to_camera"]
-        hand_instance_segmentation_image = hand_camera.get_current_frame()["instance_segmentation"]["data"]
-        hand_instance_segmentation_dict = hand_camera.get_current_frame()["instance_segmentation"]["info"]["idToSemantics"]
-        focus_distance = hand_camera.get_focus_distance()
-        horizontal_aperture = hand_camera.get_horizontal_aperture()
-        
-        print(hand_camera.get_current_frame()["instance_segmentation"])
-        
-        
-        hand_imgplot = transform(hand_rgb_image)
-       
-        # class가 2,3,4로 순서대로 나타나는게 아니라 (2,3) (3,4) 등으로 나타날 때도 있음 해당 예외 처리를 위해 다음과 같은 dict 생성 
-        class_list = {}
-        for kl in range(2,5):
-            if str(kl) in hand_instance_segmentation_dict.keys():
-                class_list[kl]=int(hand_instance_segmentation_dict[str(kl)]['class'])
-        
-        # hand_instance_segmentation_image의 경우 class(2,3,4)로 라벨이 되어있음. 이를 label로 바꿔줌
-        for c in class_list.keys():
-            np.place(hand_instance_segmentation_image, hand_instance_segmentation_image==c, class_list[c])
-        print(np.unique(hand_instance_segmentation_image))
-        # png형태로 저장
-        hand_inssegplot = Image.fromarray(hand_instance_segmentation_image)
-        if i < 100000:
-            hand_imgplot.save(args.save_path+"/train/img/img"+str(int(i/15))+".png")
-            hand_inssegplot.save(args.save_path+"/train/mask/mask"+str(int(i/15))+".png")
-        else:
-            hand_imgplot.save(args.save_path+"/val/img/img"+str(int(i/15))+".png")
-            hand_inssegplot.save(args.save_path+"/val/mask/mask"+str(int(i/15))+".png")
-            
-        for l in range(obj_num):
-            delete_prim("/World/object"+str(l))
-        
-        
-    if i == 150000:
-        simulation_app.close()
 
-    i += 1
+    #-----4.1. Randomly select object and add to the scene
+    obj_num = random.randint(1,3) # random number of objects
+    for l in range(obj_num):
+        # randomize x,y position related to world, z position is determined by object size
+        random_position = [random.random()*0.3+0.33, random.random()*0.6-0.17, 0.1]
+
+        # random object from obj_dirs
+        random_idx = random.randint(0, total_object_num-1)
+        usd_file = object_info[random_idx]['usd_file']
+        obj_label = object_info[random_idx]['label']
+        prim_path = "/World/object"+str(l) # 1st, 2nd, 3rd object
+        object_prim = create_prim(
+            usd_path=usd_file, 
+            prim_path=prim_path, 
+            position=random_position, 
+            scale=[0.2,0.2,0.2])
+        # update semantic information
+        # label 0   : unlabel 
+        # label 1   : background 
+        # label 2 ~ : YCB object 
+        add_update_semantics(prim=object_prim, semantic_label=str(l*100+obj_label))
+    
+    # about 10 steps to stablize the scene
+    for j in range(10):
+        my_world.step(render=True)
+
+    #-----4.2. Get camera image
+    hand_rgb_image = hand_camera.get_rgba()[:, :, :3]
+    hand_depth_image = hand_camera.get_current_frame()["distance_to_camera"]
+    hand_instance_segmentation_image = hand_camera.get_current_frame()["instance_segmentation"]["data"]
+    hand_instance_segmentation_dict = hand_camera.get_current_frame()["instance_segmentation"]["info"]["idToSemantics"]
+    
+    print(hand_camera.get_current_frame()["instance_segmentation"])
+    
+    #-----4.3. post-processing for instance segmentation
+    # class가 2,3,4로 순서대로 나타나는게 아니라 (2,3) (3,4) 등으로 나타날 때도 있음 해당 예외 처리를 위해 다음과 같은 dict 생성 
+    class_dict = {}
+    for kl in range(2,5):
+        if str(kl) in hand_instance_segmentation_dict.keys():
+            class_dict[kl]=int(hand_instance_segmentation_dict[str(kl)]['class'])
+    
+    # hand_instance_segmentation_image의 경우 class(2,3,4)로 라벨이 되어있음. 이를 label로 바꿔줌
+    for c in class_dict.keys():
+        np.place(hand_instance_segmentation_image, hand_instance_segmentation_image==c, class_dict[c])
+    print(np.unique(hand_instance_segmentation_image))
+
+    #-----4.4. save image
+    # png형태로 저장
+    split = "train" if i < 100000 else "val"
+    img_name = "/{}/img/img{}.png".format(split, i)
+    mask_name = "/{}/mask/mask{}.png".format(split, i)
+    save_image(hand_rgb_image, os.path.join(save_path, img_name))
+    save_image(hand_instance_segmentation_image, os.path.join(save_path, mask_name))
+        
+    #-----4.5. delete objects
+    for l in range(obj_num):
+        delete_prim("/World/object"+str(l))
