d9d8c8adb1ae27697f166e57acae16cb0abd4b12
tjdhg456 <52290290+tjdhg456@users.noreply.github.com>
2023-07-28 11:25:01 +0000

    Update 0_simulation_start.py

--- lecture/2-2/0_simulation_start.py
+++ lecture/2-2/0_simulation_start.py
@@ -7,3 +7,11 @@
 from omni.isaac.kit import SimulationApp
 simulation_app = SimulationApp({"headless": False})  # 해당 코드 실행 시, 시뮬레이션이 시작됨
 print(simulation_app.DEFAULT_LAUNCHER_CONFIG)
+
+i = 0
+
+while simulation_app.is_running():
+    i += 1
+    print(i)
+    
+simulation_app.close()
