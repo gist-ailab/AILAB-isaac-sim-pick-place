08549169460460cc23c8bad7f5d47351bc79c99e
mhko1998 <mhko1998@gist.ac.kr>
2023-07-28 23:48:30 +0900

    x

--- lecture/2-4/train_model.py
+++ lecture/2-4/train_model.py
@@ -31,7 +31,7 @@ from PIL import ImageFile
 def get_model_object_detection(num_classes):
     # load an instance segmentation model pre-trained on COCO
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=None, weights_backbone=None)
-    model.backbone.body.load_state_dict(torch.load(os.path.join(lecture_path,"1-4/parameters/resnet50.pth"),strict=False)
+    model.backbone.body.load_state_dict(torch.load(os.path.join(lecture_path,"1-4/parameters/resnet50.pth"),strict=False))
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
